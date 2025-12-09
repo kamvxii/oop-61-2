45c819ec64dccdea75a6184faf1a4a817c6f3a69
admin <kamilaakynbaeva@mail.ru>
2025-12-09 23:49:33 +0600

    m

--- control work.py
+++ control work.py
@@ -37,3 +37,70 @@ class BankAccount:
 
     def login(self, password):
         return password == self.__password
+    @property
+    def full_info(self):
+        return self._balance
+    def get_bank_name(self):
+        return self.bank_name
+
+    def bonus_for_level(self, level):
+        return self._balance - level
+
+
+kama=BankAccount("Kama", 100000, "Kama123", "Mbank")
+print(kama.get_bank_name())
+
+
+
+#4task
+class Test:
+    def __init__(self, name, level, balance):
+        self.name = name
+        self.level = level
+        self.balance = balance
+
+
+    def __str__(self):
+        return f"{self.name} | Баланс: {self.balance} SOM"
+
+    def __eq__(self, other):
+        return self.name == other.name and self.level == other.level
+    def __add__(self, other):
+        return self.name + other.name
+print(Test("Kama", 100, "Kama123"))
+
+
+
+#5 task
+from abc import ABC, abstractmethod
+
+class SmsService(ABC):
+
+    @abstractmethod
+    def send_otp(self, phone):
+        pass
+
+
+class KGSms(SmsService):
+    def send_otp(self, phone):
+        code = "1234"
+        return f"<text>Код: {code}</text><phone>{phone}</phone>"
+
+
+class RUSms(SmsService):
+    def send_otp(self, phone):
+        code = "1234"
+        return {
+            "text": f"Код: {code}",
+            "phone": phone
+        }
+
+
+
+kg = KGSms()
+ru = RUSms()
+
+print(kg.send_otp("+996500000000"))
+print(ru.send_otp("+79990000000"))
+
+
